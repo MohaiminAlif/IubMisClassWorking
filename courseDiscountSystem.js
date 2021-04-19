358fc3144eb8e753fed6b92a0c06ad4547f636d3
Mohaimin Alif <69976918+MohaiminAlif@users.noreply.github.com>
2021-04-19 23:53:39 +0600

    Update courseDiscountSystem.js

--- courseDiscountSystem.js
+++ courseDiscountSystem.js
@@ -8,18 +8,24 @@ function myFunction() {
   
     b = 2021;//current year
     age = b-year;//determining age my subtracting current year from birth year 
+    var total = x * 10000;
 
     if(age>=20 && x>=4){
-
-        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your age is "+ age;
+        dis = total * 0.25;
+        newTotal = total - dis;
+        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your tution fee is "+ newTotal;
     }
 
     else if(age>=18 && x>=3){
-        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your age is "+ age;
+        dis = total * .10;
+        newTotal = total - dis;
+        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your tution fee is "+ newTotal;
     }
 
     else{
-        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your age is "+ age;
+        dis = total * .05;
+        newTotal = total - dis;
+        document.getElementById("text").innerHTML = "Hello "+ name + " Your total course is " + x + " and your tution fee is "+ newTotal;
     }
 
 
