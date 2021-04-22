8ba133c513cbf460015a966c14bffe7b14b2eecc
Mohaimin Alif <69976918+MohaiminAlif@users.noreply.github.com>
2021-04-22 22:46:01 +0600

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -21,3 +21,24 @@ function confirm() {
     document.write("sum of numbers greater or equal to 100 is =  "+sum)
    
 }
+
+
+
+/*
+function check() {
+               
+    var fYear = parseInt(document.getElementById("fY").value);
+    var lYear = parseInt(document.getElementById("lY").value);
+
+    var count = 0;
+    
+    for(var i = fYear ; i<=lYear ; i++){
+        if(i % 4 == 0){
+            count++;
+        }
+   
+    }
+    document.write("Total number of leap year is =  "+count)
+   
+}
+*/
